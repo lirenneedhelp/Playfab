96468be6490769551f28782c290aeec0be595e07
lirenneedhelp <88997226+lirenneedhelp@users.noreply.github.com>
2023-11-23 17:40:10 +0800

    Update AccountManager.cs

--- Playfab/Assets/Script/Manager/AccountManager.cs
+++ Playfab/Assets/Script/Manager/AccountManager.cs
@@ -3,7 +3,7 @@ using PlayFab;
 using PlayFab.AdminModels;
 using UnityEngine.SceneManagement;
 using TMPro;
-
+using PlayFab.ClientModels;
 
 public class AccountManager : MonoBehaviour
 {
@@ -19,24 +19,49 @@ public class AccountManager : MonoBehaviour
                 return;
             }
 
-            var request = new DeleteMasterPlayerAccountRequest()
+            // Define the CloudScript function request
+            var request = new ExecuteCloudScriptRequest
             {
-                PlayFabId = DataCarrier.Instance.playfabID
+                FunctionName = "deletePlayerAccount", // Specify the CloudScript function name
+                FunctionParameter = new { PlayFabId = DataCarrier.Instance.playfabID }
             };
 
-            // Call the DeletePlayer API
-            PlayFabAdminAPI.DeleteMasterPlayerAccount(request, 
-            result=>{
-                Debug.Log("Account deletion successful!");
-                PlayFabClientAPI.ForgetAllCredentials();
-                //PhotonNetwork.LeaveRoom();
-                //PhotonNetwork.Disconnect();
-                Destroy(LevelSystem.Instance.gameObject);
-                SceneManager.LoadScene(0);
-            }, 
-            error=>{
-                Debug.LogError("Account deletion failed: " + error.ErrorMessage);
-            });
+            // Call the CloudScript function
+            PlayFabClientAPI.ExecuteCloudScript(request,
+                result =>
+                {
+                    Debug.Log("Account deletion successful!");
+                    PlayFabClientAPI.ForgetAllCredentials();
+                    Destroy(LevelSystem.Instance.gameObject);
+                    SceneManager.LoadScene(0);
+                    //PhotonNetwork.LeaveRoom();
+                    //PhotonNetwork.Disconnect();
+                },
+                error =>
+                {
+                    Debug.LogError("Account deletion failed: " + error.ErrorMessage);
+                });
+
+
+            //var request = new DeleteMasterPlayerAccountRequest()
+            //{
+            //    PlayFabId = DataCarrier.Instance.playfabID
+            //};
+
+
+            //// Call the DeletePlayer API
+            //PlayFabAdminAPI.DeleteMasterPlayerAccount(request, 
+            //result=>{
+            //    Debug.Log("Account deletion successful!");
+            //    PlayFabClientAPI.ForgetAllCredentials();
+            //    //PhotonNetwork.LeaveRoom();
+            //    //PhotonNetwork.Disconnect();
+            //    Destroy(LevelSystem.Instance.gameObject);
+            //    SceneManager.LoadScene(0);
+            //}, 
+            //error=>{
+            //    Debug.LogError("Account deletion failed: " + error.ErrorMessage);
+            //});
         }
     }
 }
